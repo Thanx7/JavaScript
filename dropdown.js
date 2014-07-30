f302dd1f3fc44806bf8f4a4fa3b1dba960fa3326
Thanx7 <Aliaksandr_Doubeikovsky@epam.com>
2014-07-30 17:08:54 +0300

    js

--- dropdown.js
+++ dropdown.js
@@ -17,21 +17,29 @@ document.onreadystatechange = function () {
 function MenuWriter () {
 	var topList = document.getElementById("topMenu");
 	var topListItems = topList.getElementsByTagName("li");
+	var bottomList = document.getElementById("bottomMenu");
+	var bottomListItems = bottomList.getElementsByTagName("li");
 	var counter = 0;
 	var liInterval;
 
-	function typewrite () {
-		topListItems[counter].style.display = "block";
-		counter++;
-		if (counter === topListItems.length) {
-	        clearInterval(liInterval);
-	        console.log("end");
-	    }
-	}
-
 	$( "#topMenu" ).mouseenter(function() {
-		console.log(counter);
-		liInterval = setInterval(typewrite, 20);
+		liInterval = setInterval(function () {
+			topListItems[counter].style.display = "block";
+			counter++;
+			if (counter === topListItems.length) {
+		        clearInterval(liInterval);
+		    }
+		}, 20);
+	});
+
+	$( "#bottomMenu" ).mouseenter(function() {
+		liInterval = setInterval(function () {
+			bottomListItems[counter].style.display = "block";
+			counter++;
+			if (counter === bottomListItems.length) {
+		        clearInterval(liInterval);
+		    }
+		}, 20);
 	});
 
 	$( "#topMenu" ).mouseleave(function() {
@@ -41,37 +49,16 @@ function MenuWriter () {
 		counter = 0;
 		clearInterval(liInterval);
 	});
+
+	$( "#bottomMenu" ).mouseleave(function() {
+		for (var i = 0; i < bottomListItems.length; ++i) {
+			bottomListItems[i].style.display = "none";
+		}			
+		counter = 0;
+		clearInterval(liInterval);
+	});
 }
 
 onload = function() {
 	var topMenu = new MenuWriter();
 };
-
-/*
-function topOver() {
-	var topList = document.getElementById("topMenu");
-	var topListItems = topList.getElementsByTagName("li");
-	var counter = 0;
-	var i = setInterval(animate, 100);
-
-	function animate() {
-	    	topListItems[counter].style.display = "block";
-		    counter++;
-		    if (counter === topListItems.length) {
-		        clearInterval(i);
-		        console.log("end");
-		    }
-
-		    function topOut() {
-				clearInterval(i);
-				console.log("out");
-				var topList = document.getElementById("topMenu");
-				var topListItems = topList.getElementsByTagName("li");
-
-				for (var i = 0; i < topListItems.length; ++i) {
-					topListItems[i].style.display = "none";
-				}
-			}
-	}
-}
-*/
